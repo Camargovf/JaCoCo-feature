4506a3b0030298ff1128541063f76acb428ec5a0
camargo <contato@valdeircamargo.com>
2022-11-21 17:10:18 -0300

    Feature JaCoCo Test

---
+++ src/main/java/com/camargojacoco/JaCoCoFeatureApp.java
@@ -0,0 +1,13 @@
+package com.camargojacoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaCoCoFeatureApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaCoCoFeatureApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/camargojacoco/JaCoCoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camargojacoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaCoCoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
